e482174421355fff3731df7bee04d911f25c5725
phonPtan <phongnguyen0812@gmail.com>
2021-11-08 07:19:04 +0700

    add static root

--- viewdata/settings.py
+++ viewdata/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.jons(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 MEDIA_URL = '/data/media/'
